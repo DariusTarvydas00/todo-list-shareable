74422361afed2cbc8dcf72f88ffbc516d6cdda6b
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-15 02:34:56 +0300

    test

--- todo-list-shareable-frontend/src/App.vue
+++ todo-list-shareable-frontend/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!     erfgsf asd   " />
+      <HelloWorld msg="You did it!   asdfdsf  erfgsf asd   " />
     </div>
   </header>
 
