816472311076ccfb8295e355de7ced963574fc65
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-15 05:35:30 +0300

    test

--- todo-list-shareable-backend/src/app.service.ts
+++ todo-list-shareable-backend/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('fdgsfdghfgh')
+    console.log('fdgsfdghfghdfgdfg')
     return 'Hello World!';
   }
 }
