c28942722a6b5674ef4b683d26366935ef9d0030
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-18 20:13:19 +0300

    test

--- todo-list-shareable-backend/src/main.ts
+++ todo-list-shareable-backend/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = +process.env.APP_PORT || 3000
+  app.setGlobalPrefix('api')
+  console.log('Port running on: ', port)
+
+  const options = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle('Todo APP')
+      .setDescription('Todo API documentation')
+      .setVersion('1.0')
+      .addTag('Todo')
+      .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
